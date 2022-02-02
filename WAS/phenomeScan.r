efc4fa5f6e4fa6a59cac93eb2f6ebb7fa7721215
louise <louise.millard@gmail.com>
2022-02-02 14:52:24 +0000

    remove print dir

--- WAS/phenomeScan.r
+++ WAS/phenomeScan.r
@@ -25,7 +25,6 @@ initial.options <- commandArgs(trailingOnly = FALSE)
 file.arg.name <- "--file="
 script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
 script.basename <- dirname(script.name)
-print(script.basename)
 setwd(script.basename)
 
 
